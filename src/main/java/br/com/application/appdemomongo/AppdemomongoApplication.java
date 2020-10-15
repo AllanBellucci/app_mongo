f9c52e57ce6dd469bfe96fc117f66d9b75aabedf
AllanBellucci <allanbellucci@gmail.com>
2020-10-15 16:27:23 -0300

    project created

---
+++ src/main/java/br/com/application/appdemomongo/AppdemomongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.application.appdemomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdemomongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdemomongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/application/appdemomongo/AppdemomongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.application.appdemomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdemomongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
